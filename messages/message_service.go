7ef6b30d9fd0f04cceb5f041d9d2a264a6229af9
tsvan <relaks1237@gmail.com>
2020-10-10 20:52:47 +0700

    city last letter/ help info

--- messages/message_service.go
+++ messages/message_service.go
@@ -2,45 +2,43 @@ package messages
 
 import (
 	"app/configs"
-	"app/types"
 	"app/db"
+	"app/types"
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
-	"fmt"
 )
 
 const (
-	HELP_MESSAGE = `Бот напоминаний о событии. Введите сообщение, затем время напоминания. Пример:
-	Текст сообщения. 2020-10-22 10:10`
+	HELP_MESSAGE = `Бот для получения информации о городе и игры в города.
+	Каждое сообщение воспринимает как название города за исключением команд.  
+	Вы пишете название города, бот называет город на последнюю букву, если последняя буква - ьъый, то на предпоследнюю. 
+	Проверки на соответствие города названого ботом с городом названным вами нет, каждым сообщением можно начинать игру заново `
 	NO_CITY_MESSAGE = "Нет такого города"
 	NO_CITIES_FOUND_MESSAGE = "Городов не найдено"
 )
 
-
 func HandleMessage(res *types.WebhookReqBody) {
-	if res.Message.Text == "/help" {
+	switch res.Message.Text {
+	case "/help":
 		sendMessage(res.Message.Chat.ID, HELP_MESSAGE)
-		return
-	} else {
-		city, err :=db.GetCityByName(res.Message.Text)
-		if err!=nil {
+	default:
+		city, err := db.GetCityByName(res.Message.Text)
+		if err != nil {
 			fmt.Println("нету города такого")
 			sendMessage(res.Message.Chat.ID, NO_CITY_MESSAGE)
 		} else {
-			fmt.Println(city.City, city.Region)
-			r := []rune(city.City)
-			randomCity, err := db.GetRandomCitiesByLetter(string(r[len(r)-1:]))
-			if err == nil  {
+			randomCity, err := db.GetRandomCitiesByLetter(getLetter(city.City))
+			if err == nil {
 				sendMessage(res.Message.Chat.ID, randomCity.City)
 			} else {
 				sendMessage(res.Message.Chat.ID, NO_CITIES_FOUND_MESSAGE)
 			}
 		}
-		return
 	}
 }
 
@@ -78,4 +76,13 @@ func sendMessage(chatID int64, text string) error {
 	}
 
 	return nil
+}
+
+func getLetter(word string) string {
+	replacer := strings.NewReplacer("ь", "", "ъ", "", "ы", "", "й", "")
+	word = replacer.Replace(word)
+	fmt.Println(word)
+	r := []rune(word)
+	letter := string(r[len(r)-1:])
+	return letter
 }
